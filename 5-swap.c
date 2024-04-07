5425d5fbd83cec7b25e852534c70e9720877f907
3lackrukh <nathan.rhys@atlasschool.com>
2024-04-07 03:27:46 +0000

    corrected typo in error message.

--- 5-swap.c
+++ 5-swap.c
@@ -18,7 +18,7 @@ void swap(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't swap,stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		free_stack();
 		free(tools.line);
 		fclose(tools.file);
